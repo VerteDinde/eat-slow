c181ef3d953a930a0fa3a30e0f9f984979dd3cbb
Keeley Hammond <keeleymhammond@gmail.com>
2017-03-12 14:57:54 -0700

    initial refactor in progress, saving to new branch while I figure out how to generate specific shopping lists

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ I started a Javascript bootcamp with Code Fellows in February of 2017. In order 
 
 This inspired a simple, fun side project - what if I could build an app that would let us pick from our favorite recipes, autogenerate a shopping list and let us check off those items at the grocery store in real time as my husband and I divided and conquered? Thus, the "Eat Slow" grocery list generator was born.
 
-## Tech Specs:
-HTML
-CSS
-Vanilla Javascript (possible with jQuery, but trying not to use it)
+## Tech Stack:
+* HTML
+* CSS
+* Javascript
+* jQuery
 
 ## Goals
 - To build something fun and simple that I would actually use.
---
+++ js/recipes.js
@@ -0,0 +1,53 @@
+'use strict';
+// RECIPES: Add new recipe here
+var recipes = [
+  {
+    name: 'Broccoli Chevre Soup',
+    ingredients: [
+      'unsalted butter', 'yellow onion', '2 carrots',
+      'sea salt', '3 cloves garlic', '4 cups veggie broth',
+      '2 heads broccoli', '1 bay leaf', 'black pepper',
+      'tahini', '4 oz chevre'],
+    quantity: 1
+  },
+  {
+    name: 'Superhero Muffins',
+    ingredients: [
+      'almond meal', 'rolled oats', 'cinnamon',
+      'nutmeg', 'baking soda', 'sea salt',
+      'walnuts', 'chocolate chips', '3 eggs',
+      '1 zucchini', '2 carrots', 'unsalted butter',
+      'maple syrup', 'vanilla extract'
+    ],
+    quantity: 1
+  },
+  {
+    name: 'Sausage Minestrone',
+    ingredients: [
+      '1 lb spicy Italian sausage', '3 ribs celery', '3 carrots',
+      '1 yellow onion', '3 cloves garlic', 'oregano',
+      'sea salt', '8 cups veggie broth', '3 zucchini',
+      '14 oz diced tomatoes', '14 oz cannelli beans', '3 cups penne',
+      'Parmesan'
+    ],
+    quantity: 1
+  },
+  {
+    name: 'Veggie Fried Rice',
+    ingreidents: [
+      'Saffron oil', '2 cloves garlic', 'ginger',
+      'red pepper flakes', '6 cups chopped vegetables', 'sea salt',
+      '4 cups brown rice', 'soy sauce', '2 eggs',
+    ],
+    quantity: 1
+  },
+  {
+    name: 'Sweet and Sour Chicken',
+    ingredients: [
+      'Chicken thighs', '15 oz canned pineapple', '2 green bell peppers',
+      '1 yellow onion', 'green onions', 'Sweet/sour sauce',
+    ],
+  }
+];
+
+// END RECIPES
